bb1dbe2cf9c347eca6cd3aae021dc6f9d144edf6
lenizochristian8-afk <lenizochristian8@gmail.com>
2025-09-20 17:56:53 +0800

    Chapter 2

--- resources/views/about.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout> 
-<x-slot:heading> 
-        About Page
-    </x-slot:heading> 
-
-</x-layout>
--- resources/views/contact.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout> 
-<x-slot:heading> 
-        Contact Page
-    </x-slot:heading> 
-
-</x-layout>
